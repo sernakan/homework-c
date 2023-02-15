cb8714f9dbd9519f82cc62126608070596e9c82e
Natalya <can-natasha@yzndex.ru>
2023-02-15 19:21:46 +0500

    Задача 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write ("max = ");
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.Write ("max = ");
+    Console.WriteLine(b);
+}
+
